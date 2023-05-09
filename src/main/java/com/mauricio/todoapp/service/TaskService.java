589d0428e66ea1db6921e6c687b51d2061379095
MauricioOcampo <maoocampo@hotmail.com>
2023-05-09 09:41:15 -0500

    se carga todoapp app de tareas para microempresas realizada por Mauricio Ocampo subido a git

--- src/main/java/com/mauricio/todoapp/service/TaskService.java
+++ src/main/java/com/mauricio/todoapp/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.mauricio.todoapp.service;
 
+import com.mauricio.todoapp.mapper.TaskInDTOToTask;
 import com.mauricio.todoapp.persistence.entity.Task;
 import com.mauricio.todoapp.persistence.repository.TaskRepository;
 import com.mauricio.todoapp.service.dto.TaskInDTO;
@@ -14,16 +15,23 @@ public class TaskService {
     //       que no lo vamos a inyectar sino por constructor
     private final TaskRepository repositoty;
 
+    // Aqui vamos a definir el mapper
+    private final TaskInDTOToTask mapper;
+
     // Este constructor me lo genero cuando declare arriba rivate final TaskRepository repositoty;
     // que es otra forma como de inyectar dependencias pero de una me obliga a declarar el metodo
-    // constructor que sigue.
-    public TaskService(TaskRepository repositoty) {
+    // constructor que sigue. y el mapper se creo cuando definimos TaskInDTOToTask
+    public TaskService(TaskRepository repositoty, TaskInDTOToTask mapper) {
         this.repositoty = repositoty;
+        this.mapper = mapper;
     }
 
     // Aqui en el servicio se va a recibir el TaskInDTO
     public Task createTask(TaskInDTO taskinDTO){
-        return null; //this.repositoty.save()
+        // Aqui se define task y recibe mapper
+        Task task = mapper.map(taskinDTO);
+        // Aqui ya tenemos el objeto Task mapeado mediante el mapper anterior
+        return this.repositoty.save(task);
     }
 
 
