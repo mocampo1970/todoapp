b74b677c5b6ac12bc6f2784d788588578f002a28
MauricioOcampo <maoocampo@hotmail.com>
2023-05-17 10:25:03 -0500

    se carga todoapp app de tareas para microempresas realizada por Mauricio Ocampo subido a git

--- src/main/java/com/mauricio/todoapp/controller/TaskController.java
+++ src/main/java/com/mauricio/todoapp/controller/TaskController.java
@@ -1,7 +1,28 @@
 package com.mauricio.todoapp.controller;
 
+import com.mauricio.todoapp.persistence.entity.Task;
+import com.mauricio.todoapp.service.TaskService;
+import com.mauricio.todoapp.service.dto.TaskInDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @RequestMapping define el camino de nuestro controlador
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    // Este señor no inyecta el servicio sino que lo define por eso abajo solicita el constructor
+    private final TaskService taskservice;
+
+    public TaskController(TaskService taskservice) {
+        this.taskservice = taskservice;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO){
+        return this.taskservice.createTask(taskInDTO);
+    }
+
 }
